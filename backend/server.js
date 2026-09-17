9442b1a12434862c7c8b59a475e06ab77dd2b39d
Dibakar Sarmah <dibakarsarmah10@gmail.com>
2026-09-17 13:20:12 +0530

    gitignore updated

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,6 @@ const studySessionRoutes = require("./routes/studySessionRoutes");
 const semesterRoutes = require("./routes/semesterRoutes");
 const resourceRoutes = require("./routes/resourceRoutes");
 
-dotenv.config();
 
 connectDB();
 
